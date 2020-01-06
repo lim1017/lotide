1d6b745d5247c92c02325dda857425afed26b28c
lim1017 <lim1017@hotmail.com>
2020-01-06 19:55:19 +0000

    assert equal with emojis

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✔️✔️Assertion Passed: " + actual + " === " + expected);
+  } else console.log("❌❌Do not match");
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(143, 1);
