90acd241895e03445b66860b8e453f3a5f3cf388
lim1017 <lim1017@hotmail.com>
2020-01-07 22:15:40 +0000

    moar work

--- eqArrays.js
+++ eqArrays.js
@@ -1,31 +1,27 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  console.log(actual)
-  console.log(expected)
-  for(let i=0; i<actual.length; i++){
-    if(actual[i]!==expected[i]){
-      console.log('no match')
-      return false
-    }
-  }
-  console.log('match')
-  return true
+  if (eqArrays(actual, expected)){
+    console.log("match")
+  }else console.log('no match')
 };
 
 
 const eqArrays = function(arr1, arr2){
 
-  let x = assertEqual(arr1, arr2)
-
-  return x
+  for(let i=0; i<arr1.length; i++){
+    if(arr1[i]!==arr2[i]){
+      return false
+    }
+  }
+  return true
 }
 
 
 
 
 // TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual([1, 2, 3], [1, 2, 3]) // => true
+assertEqual([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
