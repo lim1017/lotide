15087a94413629678dde4a320366cf0f93962450
lim1017 <lim1017@hotmail.com>
2020-01-08 00:03:31 +0000

    asdf

---
+++ flatten.js
@@ -0,0 +1,32 @@
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔️✔️Assertion Passed: ${actual} ===  ${expected}`);
+  } else console.log(`❌❌Do not match ${actual} !===  ${expected}`);
+};
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const flatten = function(arr){
+let flat=[]
+  for(let i=0; i<arr.length; i++){
+    if(Array.isArray(arr[i])){
+      for(let j=0; j<arr[i].length; j++){
+        flat.push(arr[i][j])
+      }
+    } else flat.push(arr[i])
+  }
+
+  return flat
+}
+
+
+
+console.log(flatten([1, 2, [3, 4, 7], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
