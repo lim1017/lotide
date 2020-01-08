c4c31d54b87a5b254ab27e0002ff6acee732a941
lim1017 <lim1017@hotmail.com>
2020-01-08 19:01:10 +0000

    count letters

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️Assertion Passed: ${actual} ===  ${expected}`);
+  } else console.log("❌❌Do not match");
+};
+
+
+const countLetters = function (countThisString){
+ let finalOP={}
+ revisedString=countThisString.toLowerCase()
+ nospace=revisedString.replace(/ /g,'')
+ 
+ for(let letter of nospace){
+  if (finalOP[letter]) {
+    finalOP[letter] += 1;
+  } else {
+    finalOP[letter] = 1;
+  }
+ }
+
+ return finalOP
+}
+
+console.log(countLetters("lighthouse in the house"))
