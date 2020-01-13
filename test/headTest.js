eaba1816c93dfed632145758866f5f71e87965d4
lim1017 <lim1017@hotmail.com>
2020-01-13 19:05:37 +0000

    added testing code

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,31 @@
 const head = require('../head')
 const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+
+
+
+describe("#head", () => {
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]),5);
+  });
+
+  it('returns Hello for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),'Hello');
+  });
+
+  it('returns Hello for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),'Hello');
+  });
+
+
+});
 
 
 
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Helloss"]), "Helloss");
-assertEqual(head([]), "Helloss");
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head(["Helloss"]), "Helloss");
+// assertEqual(head([]), "Helloss");
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,26 @@
 const tail = require('../tail')
 const assertArrayEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 
+describe("#tail", () => {
+
+
+  it("returns [2,3,4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]),[2,3,4]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+})
 
 
 assertArrayEqual(tail([1, 2, 3, 4]),[2,3,4])
